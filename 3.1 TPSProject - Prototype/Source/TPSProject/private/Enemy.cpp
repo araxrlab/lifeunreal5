e0c7c7cce1a38b1ebb321451674fd4c5c16924c9
f11m4at4 <f11m4at4@gmail.com>
2024-06-04 23:22:22 +0900

    인생 언리얼5 프로젝트 교과서 3장 프로토타입 프로젝트
    인생 언리얼5 프로젝트 교과서 3장 프로토타입 프로젝트

---
+++ 3.1 TPSProject - Prototype/Source/TPSProject/private/Enemy.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy.h"
+#include "EnemyFSM.h"
+
+// Sets default values
+AEnemy::AEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 1. 스켈레탈메시 데이터 로드
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> tempMesh(TEXT("SkeletalMesh'/Game/Characters/Mannequin_UE4/Meshes/SK_Mannequin.SK_Mannequin'"));
+	// 1-1. 데이터 로드가 성공하면
+	if (tempMesh.Succeeded())
+	{
+		// 1-2. 데이터 할당
+		GetMesh()->SetSkeletalMesh(tempMesh.Object);
+		// 1-3. 메시 위치 및 회전 설정
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0, 0, -88), FRotator(0, -90, 0));
+	}
+
+	// EnemyFSM 컴포넌트 추가
+	fsm = CreateDefaultSubobject<UEnemyFSM>(TEXT("FSM"));
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+}
+
+// Called to bind functionality to input
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
